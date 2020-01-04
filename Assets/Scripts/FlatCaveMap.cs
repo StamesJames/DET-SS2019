b299cadfa2ee477208b6d526e0b76d4f8fc6469d
StamesJames <benedict.smit@udo.edu>
2020-01-04 17:09:42 +0100

    Flatcave etwas allgemeiner
    Flatcave ist nun etwas allgemeiner und so, dass von außden parameter geändert werden können wenn diese sichtbar gemacht werden nach außen

--- Assets/Scripts/FlatCaveMap.cs
+++ Assets/Scripts/FlatCaveMap.cs
@@ -1,139 +1,153 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
+﻿using UnityEngine;
 
+[System.Serializable]
 public class FlatCaveMap : MapGenerator
 {
 
-    public int[,,] layout = new int[World.chunkSize * 3, World.chunkSize * 3, World.chunkSize * 3];
-    public int randomFillPercent = 40;
-    int width = World.chunkSize*3;
-    int height = World.chunkSize*3;
+    private Block.BlockType[,] flatMap;
 
-    int länge = World.chunkSize*3;
-    
+    [SerializeField] private int randomFillPercent = 40;
+    [SerializeField] private int smoothingItterations = 3;
+    [SerializeField] private Intervall[] birthIntervalls;
+    [SerializeField] private Intervall[] deathIntervalls; 
+
 
     public FlatCaveMap()
     {
         xChunkCount = 3;
         yChunkCount = 3;
         zChunkCount = 3;
-        currentBlockMap = new Block.BlockType[länge, länge, länge];
-
-        RandomFillMap();
-
-        for(int i =0;i<1;i++) SmoothMap();
-
-        for (int i = 0; i < länge; i++)
-            for (int j = 0; j < länge; j++)
-                for (int k = 0; k < länge; k++)
-                {
-                    if(layout[i,j,3]==1)
-                    {
-                        currentBlockMap[i, k, j] = Block.BlockType.DIAMOND;
-                    }else{
-                        currentBlockMap[i, k, j] = Block.BlockType.AIR;
-                    }
-                }
-
-        CreaterBottomCeling();
+        birthIntervalls = new Intervall[] { new Intervall(5, 8) };
+        deathIntervalls = new Intervall[] { new Intervall(0, 3) };
+        GenerateMap();
     }
 
     public override Block.BlockType[,,] GenerateMap()
     {
+        flatMap = new Block.BlockType[xChunkCount * World.chunkSize, yChunkCount * World.chunkSize];
+        currentBlockMap = new Block.BlockType[xChunkCount * World.chunkSize, yChunkCount * World.chunkSize, zChunkCount * World.chunkSize];
+        RandomFillMap();
+
+        for (int i = 0; i < 1; i++) SmoothMap();
+
+        for (int x = 0; x < xChunkCount * World.chunkSize; x++)
+            for (int y = 0; y < yChunkCount * World.chunkSize; y++)
+                for (int z = 0; z < zChunkCount * World.chunkSize; z++)
+                {
+                    currentBlockMap[x, y, z] = flatMap[x, y];
+                }
+        CreaterBottomCeling();
+
         return currentBlockMap;
     }
 
-
-
-
-
     void RandomFillMap()
     {
-
-        System.Random pseudoRandom = new System.Random();
-        
-
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
+        System.Random pseudoRandom = new System.Random();       
+        for (int x = 0; x < xChunkCount * World.chunkSize; x++)
+            for (int y = 0; y < yChunkCount * World.chunkSize; y++)
             {
-                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                if (x == 0 || x == xChunkCount * World.chunkSize - 1 || y == 0 || y == yChunkCount * World.chunkSize - 1)
                 {
-                    layout[x, y,3] = 1;
+                    flatMap[x, y] = Block.BlockType.STONE;
                 }
                 else
                 {
-                    layout[x, y,3] = (pseudoRandom.Next(1,100) < randomFillPercent) ? 1 : 0;
-                    Debug.Log(pseudoRandom.Next(1,100));
+                    flatMap[x, y] = (pseudoRandom.Next(1,100) < randomFillPercent) ? Block.BlockType.STONE : Block.BlockType.AIR;
                 }
-            }
-        }
+            }       
     }
 
     void SmoothMap() {
-		for (int x = 0; x < width; x ++) {
-			for (int y = 0; y < height; y ++) {
-				int neighbourWallTiles = GetSurroundingWallCount(x,y,3);
-
-				if (neighbourWallTiles > 4)
-					layout[x,y,3] = 1;
-				else if (neighbourWallTiles < 4)
-					layout[x,y,3] = 0;
-
-			}
-		}
-	}
-
-    int GetSurroundingWallCount(int gridX, int gridY, int gridZ) 
-    {
-        int wallCount = 0;
-        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
-        {
-            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+        Block.BlockType[,] newFlatMap = new Block.BlockType[xChunkCount * World.chunkSize, yChunkCount * World.chunkSize];
+		for (int x = 0; x < xChunkCount * World.chunkSize; x ++)
+			for (int y = 0; y < yChunkCount * World.chunkSize; y ++)
             {
-                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+				int neighboreCount = CountSurroundingBlocks(x,y,Block.BlockType.STONE,flatMap,1);
+                newFlatMap[x, y] = flatMap[x, y];
+                if (flatMap[x,y] == Block.BlockType.STONE)
                 {
-                    if (neighbourX != gridX || neighbourY != gridY)
+                    foreach (Intervall deathIntervall in deathIntervalls)
                     {
-                        wallCount += layout[neighbourX, neighbourY,3];
+                        if (deathIntervall.Contains(neighboreCount))
+                        {
+                            newFlatMap[x, y] = Block.BlockType.AIR;
+                        }
                     }
                 }
                 else
                 {
-                    wallCount++;
+                    foreach (Intervall birthIntervall in birthIntervalls)
+                    {
+                        if (birthIntervall.Contains(neighboreCount))
+                        {
+                            newFlatMap[x, y] = Block.BlockType.STONE;
+                        }
+                    }
+                }
+			}
+        flatMap = newFlatMap;
+	}
+
+    int CountSurroundingBlocks(int gridX, int gridY, Block.BlockType blockType, Block.BlockType[,] map, int distance) 
+    {
+        int blockCount = 0;
+        for (int neighbourX = gridX - distance; neighbourX <= gridX + distance; neighbourX++)        
+            for (int neighbourY = gridY - distance; neighbourY <= gridY + distance; neighbourY++)
+            {
+                if (neighbourX >= 0 && neighbourX < xChunkCount * World.chunkSize && neighbourY >= 0 && neighbourY < yChunkCount * World.chunkSize)
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        blockCount += flatMap[neighbourX,neighbourY] == blockType ? 1 : 0;
+                    }
+                }
+                else
+                {
+                    blockCount++;
                 }
             }
-        }
-
-        return wallCount;
+        return blockCount;
     }
 
     void CreaterBottomCeling(){
-        for (int i = 0; i < länge; i++){
-            for (int j = 0; j < länge; j++){
-                for (int k = 0; k < länge; k++){
+        for (int i = 0; i < xChunkCount * World.chunkSize; i++)
+            for (int j = 0; j < yChunkCount * World.chunkSize; j++)
+                for (int k = 0; k < zChunkCount * World.chunkSize; k++){
                     currentBlockMap[i, 1, j] = Block.BlockType.DIAMOND;
                     currentBlockMap[i, 2, j] = Block.BlockType.DIAMOND;
 
                 }
-            }
-        }
-        for (int i = länge/2; i>1; i--){
-            for (int j = 0; j < länge; j++){
-                for (int k = 0; k < länge; k++){
+            
+        
+        for (int i = xChunkCount * World.chunkSize / 2; i>1; i--)
+            for (int j = 0; j < yChunkCount * World.chunkSize; j++)
+                for (int k = 0; k < zChunkCount * World.chunkSize; k++){
                     if(currentBlockMap[j, i, k]==Block.BlockType.DIAMOND)
                     {
                         currentBlockMap[j,i-1,k] = Block.BlockType.DIRT;
                     }
                 }
-            }
-        }
+            
+        
     }
 
+}
 
+[System.Serializable]
+public class Intervall
+{
+    public float von;
+    public float bis;
 
+    public Intervall(float pVon, float pBis)
+    {
+        von = pVon;
+        bis = pBis;
+    }
 
-
+    public bool Contains(float x)
+    {
+        return (x >= von && x <= bis);
+    }
 }
