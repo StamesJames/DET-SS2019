127c2525cc63e65c7de3716ee43dc10d1dcb193e
StamesJames <benedict.smit@udo.edu>
2020-01-06 23:51:24 +0100

    3D Caves können nun erzuegt werdne
    3D caves können nun erzeugt werden und sind an die UI angeschlossen.

---
+++ Assets/Scripts/MapGeneration/Holder/ThreeDCaveHolder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ThreeDCaveHolder : MapGeneratorHolder
+{
+    [SerializeField] private TMP_InputField xChunkCountInput;
+    [SerializeField] private TMP_InputField yChunkCountInput;
+    [SerializeField] private TMP_InputField zChunkCountInput;
+    [SerializeField] private Slider randomFillPercentInput;
+    [SerializeField] private TMP_InputField itterationCountInput;
+    [SerializeField] private TextMeshProUGUI percentAnzeige;
+
+    private ThreeDCave generator = new ThreeDCave();
+
+    private void Start()
+    {
+        UpdateValues();
+    }
+
+    public override MapGenerator GetGenerator()
+    {
+        return generator;
+    }
+
+    public void UpdateChunkCountSize()
+    {
+        generator.XChunkCount = int.Parse(xChunkCountInput.text);
+        generator.YChunkCount = int.Parse(yChunkCountInput.text);
+        generator.ZChunkCount = int.Parse(zChunkCountInput.text);
+    }
+
+    public override void UpdateValues()
+    {
+        UpdateChunkCountSize();
+        generator.RandomFillPercent = (int)randomFillPercentInput.value;
+        generator.SmoothingItterations = int.Parse(itterationCountInput.text);
+        percentAnzeige.text = randomFillPercentInput.value.ToString() + "%";
+    }
+}
